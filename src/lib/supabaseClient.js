43af3154ebea6543e54d283e56047f34aef9c354
dragoontv <dragoon.tv21@gmail.com>
2025-09-17 00:58:35 +0700

    Fix Supabase Error - Final

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,9 +1,14 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
-export const supabase =
-  supabaseUrl && supabaseAnonKey
-    ? createClient(supabaseUrl, supabaseAnonKey)
-    : null
+export const getSupabaseClient = () => {
+    const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+    const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+
+    if(!supabaseUrl || !supabaseAnonKey) {
+        throw new Error("Missing Supabase credentials");
+    }
+    return createClient(supabaseUrl, supabaseAnonKey);
+};
+
+export const supabase = getSupabaseClient();
